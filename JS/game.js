1a8f2ac6ec7ac530dfde422b480dfbf09c82c115
Sebastián Peña <fuzzdev@Sebastian>
2023-04-22 08:46:03 -0400

    se genera la logica de renderizado del mapa, las logicas del movimiento del jugador y su limitacion de movimiento dentro del canvas

--- JS/game.js
+++ JS/game.js
@@ -11,12 +11,102 @@ const map = maps[rdm];
 const mapsRows = map.trim().split("\n");
 const elementsMap = mapsRows.map((row) => row.trim().split(""));
 
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
+
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
+
+let canvasSize;
+let elementsSize;
+
 console.log(elementsMap);
 
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
-let canvasSize;
-let elementsSize;
+
+document.addEventListener("keydown", (e) => {
+  let key = e.key;
+
+  switch (key) {
+    case "ArrowUp":
+      moveUp();
+      break;
+
+    case "ArrowLeft":
+      moveLeft();
+      break;
+
+    case "ArrowRight":
+      moveRight();
+      break;
+
+    case "ArrowDown":
+      moveDown();
+      break;
+
+    default:
+      break;
+  }
+});
+
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
+
+function moveUp() {
+  console.log(canvasSize);
+  console.log(playerPosition.y);
+
+  if (playerPosition.y <= (canvasSize / 10) * 2) {
+    playerPosition.y = (canvasSize / 10) * 2;
+  }
+  playerPosition.y -= elementsSize;
+  clearMap();
+  movePlayer();
+}
+
+function moveLeft() {
+  console.log(canvasSize);
+  console.log(playerPosition.x);
+  if (playerPosition.x <= canvasSize / 10) {
+    playerPosition.x = elementsSize * 2;
+  }
+  playerPosition.x -= elementsSize;
+
+  clearMap();
+  movePlayer();
+}
+
+function moveRight() {
+  console.log(canvasSize);
+  console.log(playerPosition.x);
+
+  if (playerPosition.x >= canvasSize) {
+    playerPosition.x = canvasSize - elementsSize;
+  }
+
+  playerPosition.x += elementsSize;
+  clearMap();
+  movePlayer();
+}
+
+function moveDown() {
+  console.log(canvasSize);
+  console.log(playerPosition.y);
+
+  if (playerPosition.y >= canvasSize) {
+    playerPosition.y = playerPosition.y - elementsSize;
+  }
+  playerPosition.y += elementsSize;
+  clearMap();
+  movePlayer();
+}
 
 function startGame() {
   game.font = elementsSize + "px sans-serif";
@@ -25,11 +115,25 @@ function startGame() {
   elementsMap.forEach((row, rowI) => {
     row.forEach((col, colI) => {
       const emoji = emojis[col];
-      const posX = elementsSize * (colI + 1)
-      const posY = elementsSize * (rowI + 1)
-      game.fillText(emoji, posX , posY);
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+
+      if (col === "O" && playerPosition.x === undefined) {
+        playerPosition.x = posX;
+        playerPosition.y = posY;
+      } else if (
+        playerPosition.x == !undefined ||
+        playerPosition.y == !undefined
+      ) {
+        playerPosition.x = playerPosition.x;
+        playerPosition.y = playerPosition.y;
+      }
+
+      game.fillText(emoji, posX, posY);
     });
   });
+
+  movePlayer();
 
   //   for (let row = 1; row <= 10; row++) {
   //     for (let col = 1; col <= 10; col++) {
@@ -41,12 +145,31 @@ function startGame() {
   //     }
   //   }
 }
+function clearMap() {
+  game.clearRect(0, 0, canvasSize, canvasSize);
+  game.font = elementsSize + "px sans-serif";
+  game.textAlign = "end";
+
+  elementsMap.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+
+      game.fillText(emoji, posX, posY);
+    });
+  });
+}
+
+function movePlayer() {
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
 
 function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
-    canvasSize = window.innerWidth * 0.8;
+    canvasSize = window.innerWidth * 0.6;
   } else {
-    canvasSize = window.innerHeight * 0.8;
+    canvasSize = window.innerHeight * 0.6;
   }
 
   canvas.setAttribute("width", canvasSize);
